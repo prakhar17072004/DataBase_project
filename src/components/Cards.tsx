2156a74a66901246422df1491420e81d65dd392b
prakhar17072004 <pmankur2004@gmail.com>
2025-01-24 22:32:25 +0530

    cards are addded

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -6,22 +6,22 @@ function Cards() {
   return (
     <div className='mt-8'>
         <div className='grid grid-cols-3 max-w-[1250px] ml-[190px] gap-4 rounded-md text-center'>
-            <div className='bg-slate-400 rounded-md'>
-            <div className='m-8 bg-cyan-800 rounded-full text-xl'>
+            <div className='bg-slate-400 rounded-3xl'>
+            <div className='m-12 bg-cyan-800 rounded-full text-xl'>
             <Image src={pic} alt ="pic" className=" p-4 mx-auto"/>
             </div>
             <h2>Auther Name:<span>Jai shanker prashad</span></h2>
             <h2>Books Name:<span></span></h2>
             </div>
-            <div className='bg-slate-400'>
-            <div className='m-8 bg-cyan-800 rounded-full text-xl'>
+            <div className='bg-slate-400 rounded-3xl'>
+            <div className='m-12 bg-cyan-800 rounded-full text-xl'>
             <Image src={pic} alt ="pic" className=" p-4 mx-auto"/>
             </div>
             <h2>Auther Name:<span>Jai shanker prashad</span></h2>
             <h2>Books Name:<span></span></h2>
             </div>
-            <div className='bg-slate-400'>
-            <div className='m-8 bg-cyan-800 rounded-full text-xl'>
+            <div className='bg-slate-400 rounded-3xl'>
+            <div className='m-12 bg-cyan-800 rounded-full text-xl'>
             <Image src={pic} alt ="pic" className=" p-4 mx-auto"/>
             </div>
                 <h2>Auther Name:<span>Jai shanker prashad</span></h2>
