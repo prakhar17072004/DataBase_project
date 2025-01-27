871e8c58865f84a8996d6e1ecd112587803a339b
prakhar17072004 <pmankur2004@gmail.com>
2025-01-27 20:32:06 +0530

    new added pages

---
+++ src/pages/authors.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AuthersPage() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-2xl font-bold">Authers List</h1>
+      <p>Here, you will find a list of all authors.</p>
+    </div>
+  );
+}
